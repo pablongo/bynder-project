908398e5c1735fac9dd9b6c5b9f227b42e432c9c
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-23 12:54:33 +0200

    Append CharacterList to Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import CharacterList from '../../components/CharacterList/CharacterList';
 
 export default function Dashboard() {
+  const characters = useSelector((store) => store.characters);
+
   return (
-    <h1>This is a Dashboard</h1>
+    <>
+      <h1>This is a Dashboard</h1>
+      <CharacterList characters={characters} />
+    </>
   );
 }
