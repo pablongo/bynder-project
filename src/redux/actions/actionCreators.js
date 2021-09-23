336e68213c9923c8765cdc6d7e7fc8c7f99744eb
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-23 10:46:26 +0200

    Create actionCreators

---
+++ src/redux/actions/actionCreators.js
@@ -0,0 +1,9 @@
+import actionTypes from './actionTypes';
+
+export async function loadCharacters() {
+  return '';
+}
+
+export async function loaddCharacters() {
+  return '';
+}
--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,3 +1,3 @@
 export default {
-
+  LOAD__CHARACTERS: 'LOAD__CHARACTERS',
 };
