75623aeaadc82b592fe179f66a9d7ec860b6fb9d
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-25 10:49:14 +0200

    Test PaginationButtons implementations

--- src/components/PaginationButtons/PaginationButtons.test.jsx
+++ src/components/PaginationButtons/PaginationButtons.test.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import { render, screen } from '../../utils/test.utils';
+import { render, screen, fireEvent } from '../../utils/test.utils';
 
 import PaginationButton from './PaginationButtons';
+
+import { loadCharacters } from '../../redux/actions/actionCreators';
 
 jest.mock('../../redux/actions/actionCreators');
 
 describe('Given a PaginationButtons component', () => {
   describe('When is rendered', () => {
     beforeEach(() => {
-      render(<PaginationButton previous="a" next="a" />);
+      render(<PaginationButton previous="previous" next="next" />);
     });
     test('Then Prev should be in the document', () => {
       expect(screen.getByText(/Prev/i)).toBeInTheDocument();
     });
     test('Then Next should be in the document', () => {
       expect(screen.getByText(/Next/i)).toBeInTheDocument();
+    });
+    describe('And prev-button is clicked', () => {
+      test('Then loadCharacters should have been caled with previous', () => {
+        const prevButton = screen.getByTestId('prev-button');
+
+        loadCharacters.mockReturnValue({ type: 'string' });
+        fireEvent.click(prevButton);
+
+        expect(loadCharacters).toHaveBeenCalledWith('previous');
+      });
+    });
+    describe('And next-button is clicked', () => {
+      test('Then loadCharacters should have been caled with previous', () => {
+        const nextButton = screen.getByTestId('next-button');
+
+        loadCharacters.mockReturnValue({ type: 'string' });
+        fireEvent.click(nextButton);
+
+        expect(loadCharacters).toHaveBeenCalledWith('next');
+      });
     });
   });
 });
